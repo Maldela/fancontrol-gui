224b9c3a471df34ac2f3642ca81cc052e2545377
Maldela <maldela@halloarsch.de>
2015-10-14 22:33:02 +0200

    Delete CTestTestfile.cmake

--- kcm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maldela/Fancontrol-GUI/kcm
-# Build directory: /home/maldela/Fancontrol-GUI/kcm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
