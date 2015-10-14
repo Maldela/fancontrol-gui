4d78abc3da8071f676e0cab6775b66d80b04b573
Maldela <maldela@halloarsch.de>
2015-10-14 22:30:34 +0200

    Delete CTestTestfile.cmake

--- helper/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maldela/Fancontrol-GUI/helper
-# Build directory: /home/maldela/Fancontrol-GUI/helper
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
